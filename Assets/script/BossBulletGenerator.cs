15cd53a67bdbd15e20a1f0345024d81068e90956
MasanoriFukuda2312 <uni202312_08@telework.to>
2024-05-11 18:44:44 +0900

    砲台の方向から弾を飛ばす

---
+++ Assets/script/BossBulletGenerator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossBulletGenerator : MonoBehaviour
+{
+    [SerializeField]
+    GameObject enemybullet;
+    [SerializeField]
+    GameObject foundation;
+    [SerializeField]
+    Transform bulletPoint;
+    [SerializeField]
+    Transform bulletPoint2;
+    GameObject EnemyBullet;
+    GameObject EnemyBullet2;
+    float span = 1.0f;
+    float delta = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.delta += Time.deltaTime;
+        if (delta > span)
+        {
+            delta = 0;
+
+            EnemyBullet = Instantiate(enemybullet, bulletPoint.transform.position, bulletPoint.transform.rotation);
+            EnemyBullet2 = Instantiate(enemybullet, bulletPoint2.transform.position, bulletPoint2.transform.rotation);
+            Debug.Log(transform.position);
+        }
+    }
+}
